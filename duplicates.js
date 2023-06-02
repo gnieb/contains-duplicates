049715391b923813db2f61af329c5d0f81f1d9fd
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-02 19:44:33 -0400

    algorithm solution coded, with my own test cases included in the duplicates,js

---
+++ duplicates.js
@@ -0,0 +1,30 @@
+function containsDuplicate (arr) {
+    let store = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] in store) {
+            return true
+        }
+        store.push(arr[i])
+    }
+    return false
+}
+
+
+if (require.main === module) {
+    // add your own tests in here
+    console.log("Expecting: true");
+    console.log("=>", containsDuplicate([11, 4, 7, 2, 3, 7]) );
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", containsDuplicate([1, 2, 3, 4]));
+  }
+
+
+module.exports = containsDuplicate;
+
+
+
+// return true if there is more than one element in the array. 
+// can iterate through the arrya and iterate a second time OR create a hashmap. 
